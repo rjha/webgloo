2c1c805215b642888fa826c75dfe00ea4f204abf
rjha <jha.rajeev@gmail.com>
2012-09-29 22:07:34 +0530

    bug fix: selectbox code and display changed to ui_code and name

--- lib/com/indigloo/ui/SelectBox.php
+++ lib/com/indigloo/ui/SelectBox.php
@@ -16,16 +16,16 @@ namespace com\indigloo\ui {
             $showEmpty = Util::tryArrayKey($options,'empty');
 
             if(!is_null($showEmpty) && $showEmpty) {
-                array_unshift($rows,array('code' => '', 'display' => '--'));
+                array_unshift($rows,array('ui_code' => '', 'name' => '--'));
             }
 
             $buffer = '' ;
-            $option = '<option value="{code}" {flag}> {display}</option>' ;
+            $option = '<option value="{ui_code}" {flag}> {name}</option>' ;
             
             foreach($rows as $row) {
 
-                $flag = (!is_null($default) && ($row['code'] == $default))? 'selected' : '' ;
-                $str = str_replace(array("{code}","{display}","{flag}") ,
+                $flag = (!is_null($default) && ($row['ui_code'] == $default))? 'selected' : '' ;
+                $str = str_replace(array("{ui_code}","{name}","{flag}") ,
                                    array($row['ui_code'], $row['name'],$flag) , $option);
                 $buffer = $buffer.$str ;
                                          
