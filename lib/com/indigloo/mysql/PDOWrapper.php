bdb54fa0143510bf48ec73cfc5886f2aec264dc5
rjha <jha.rajeev@gmail.com>
2012-11-17 21:22:47 +0530

    comments added

--- lib/com/indigloo/mysql/PDOWrapper.php
+++ lib/com/indigloo/mysql/PDOWrapper.php
@@ -3,6 +3,22 @@ namespace com\indigloo\mysql{
 
     use \com\indigloo\Configuration as Config ;
     
+    /*
+     * to close the connx to database, set $dbh = NULL inside your application code
+     * from the PHP manual 
+     * ------------------------------------------
+     * 
+     * Upon successful connection to the database, an instance of the PDO class
+     * is returned to your script. The connection remains active for the lifetime
+     * of that PDO object. To close the connection, you need to destroy the object 
+     * by ensuring that all remaining references to it are deleted--you do this by 
+     * assigning NULL to the variable that holds the object. If you don't do this 
+     * explicitly, PHP will automatically close the connection when your script ends.
+     * 
+     * ---------------------------------------------
+     *
+     */
+
     class PDOWrapper {
 
         static function getHandle() {
