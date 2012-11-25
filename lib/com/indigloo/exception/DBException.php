5090d5fc18d19381bac13cd19df1026612a7acb8
rjha <jha.rajeev@gmail.com>
2012-11-26 02:30:56 +0530

    - code should be an integer before passing it to PHP \Exception class.  - some callers can return non-integer code (e.g. PDOExceptions)

--- lib/com/indigloo/exception/DBException.php
+++ lib/com/indigloo/exception/DBException.php
@@ -4,8 +4,11 @@ namespace com\indigloo\exception {
 
     class DBException extends \Exception  {
 
-        public function __construct($message,$code=0) {
-            parent::__construct($message,$code);
+        public function __construct($message,$code=0, \Exception $previous = null) {
+            // PDO exception etc. can return strange string codes
+            // Exception expects an integer error code.
+            settype($code,"integer");
+            parent::__construct($message,$code,$previous);
         }
 
     }
